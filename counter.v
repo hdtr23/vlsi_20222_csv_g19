785fbaaf6c3205c79e634ab7f9207c4e3f727a7e
hdtr23 <hien.td192833@sis.hust.edu.vn>
2023-07-09 23:58:24 +0700

    new

---
+++ counter.v
@@ -0,0 +1,54 @@
+module counter(
+	input clk,
+	input rst_n,
+	input num_i,
+	output reg of,
+	output reg [7:0] cnt
+);
+parameter A=3'b000, B=3'b001, C=3'b011, D=3'b010, E=3'b110; 
+reg [2:0] next_state;
+reg [2:0] current_state;
+initial begin
+	cnt = 0;
+	of = 0;
+end
+always @(posedge clk) begin:RESET
+	if(!rst_n) begin 
+		of <= 1'b0;
+		cnt <= 8'b00000000;
+	end
+end
+always @(posedge clk) begin:FSM
+current_state = next_state;
+case(current_state)
+	A: if(num_i == 0)
+		next_state = B;
+		else next_state = A;
+	B: if(num_i == 0)
+		next_state = B;
+		else next_state = C;
+	C: if(num_i == 0)
+		next_state = D;
+		else next_state = A;
+	D: if(num_i == 0)
+		next_state = B;
+		else next_state = E;
+	E: if(num_i == 0)
+		next_state = D;
+		else next_state = A;
+	default : begin 
+		current_state = A;
+		next_state = A;
+	end
+endcase
+
+end
+
+always @(posedge clk) begin: COUNTER
+	if(cnt == 8'b11111111) 
+		of <= 1'b1;
+	if(!of && current_state == E)
+		cnt <= cnt + 1'b1;
+end
+endmodule
+
---
+++ counter_tb.v
@@ -0,0 +1,48 @@
+`timescale 1ns/1ps
+
+module counter_tb();
+
+reg clk_tb;
+reg rst_n_tb;
+reg num_i_tb;
+wire of_tb;
+wire [7:0] cnt_tb;
+
+initial begin
+    clk_tb = 0;
+    forever begin
+        clk_tb = !clk_tb;
+        #5;
+    end
+end
+
+initial begin
+    rst_n_tb = 1;
+    #1000;
+    rst_n_tb = 0;
+    #10;
+    rst_n_tb = 1;
+    #5300;
+    rst_n_tb = 0;
+    #10;
+    rst_n_tb = 1;
+end
+
+initial begin
+    num_i_tb = 0;
+    forever begin
+        num_i_tb = !num_i_tb;
+        #10;
+    end
+end
+
+
+counter cnt(
+.clk(clk_tb),
+.rst_n(rst_n_tb),
+.num_i(num_i_tb),
+.of(of_tb),
+.cnt(of_tb)
+);
+
+endmodule
